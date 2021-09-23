9df1fdb0dd38ce2ba78e31ca073ce1e062055da4
Otávio Lube <otaviolube@gmail.com>
2021-09-23 13:11:00 -0300

    Iniciando o jogo do bingo

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let vetorJogadores = [];
+
 function gerarNumeroAleatorio(inicioIntervalo, fimIntervalo) {
     return Math.floor((fimIntervalo - inicioIntervalo) * Math.random()) + inicioIntervalo;
 }
@@ -19,23 +21,38 @@ function gerarFileira(quantidadeNumeros, inicio, fim) {
     return cartela;
 }
 
-function gerarCartela(){
+function gerarCartela() {
     return [gerarFileira(5, 1, 15), gerarFileira(5, 16, 30), gerarFileira(5, 31, 45), gerarFileira(5, 46, 60), gerarFileira(5, 61, 75)];
 }
 
 function gerarCartelaHTML() {
 
-    let nomeJogador = prompt("Digite o nome do jogador:;")
+    let nomeJogador = prompt("Digite o nome do jogador:");
+    if(nomeJogador == "" || nomeJogador == null){
+        alert("Você precisa digitar o nome do jogador!!!");
+        return;
+    }else if(nomeJogador.length <= 5){
+        alert("O nome é muito pequeno!!!");
+        return;
+    }
 
     let titulo = ['B', 'I', 'N', 'G', 'O'];
     let cartela = gerarCartela();
+
+    let jogador = {
+        nome: nomeJogador,
+        cartela: cartela
+    }
+
+    vetorJogadores.push(jogador);
+    console.log(vetorJogadores);
 
     let divBingo = document.getElementById("bingo");
 
     let h3nome = document.createElement("h3");
     h3nome.style = "text-align: center;"
     h3nome.innerText = nomeJogador;
-    
+
 
     let divCartela = document.createElement("div");
     divCartela.appendChild(h3nome);
@@ -54,14 +71,14 @@ function gerarCartelaHTML() {
 
     table.appendChild(tr);
 
-    for(let linha = 0; linha < 5; linha++){
+    for (let linha = 0; linha < 5; linha++) {
         let tr = document.createElement("tr");
-        for(let coluna = 0; coluna < 5; coluna++){
+        for (let coluna = 0; coluna < 5; coluna++) {
             let td = document.createElement("td");
             td.className = "borda-tabela";
-            if(linha == 2 && coluna == 2){
+            if (linha == 2 && coluna == 2) {
                 td.innerText = "X";
-            }else{
+            } else {
                 td.innerText = cartela[coluna][linha];
             }
             tr.appendChild(td);
@@ -74,4 +91,59 @@ function gerarCartelaHTML() {
     divCartela.style = "display: inline;"
     divBingo.appendChild(divCartela);
 
+}
+
+function verificaCartela(cartela, numerosSorteados, quantidadeNumeros) {
+    if (numerosSorteados.length < quantidadeNumeros) {
+        return false;
+    }
+    let numerosExistem = true;
+    cartela.forEach(function (numero) {
+        if (numerosSorteados.includes(numero) == true) {
+            numerosExistem = true;
+        } else {
+            numerosExistem = false;
+            return false;
+        }
+    });
+    if (numerosExistem == true) {
+        return true;
+    }
+}
+
+function jogarBingo() {
+    if(vetorJogadores.length < 2){
+        alert("Você precisa ter pelo menos dois jogadores para jogar!!!");
+        return;
+    }
+    let numerosSorteados = [];
+    let divSorteados = document.getElementById("sorteados");
+    let intervalo = setInterval(function () {
+        let numeroExiste = true;
+        while (numeroExiste == true) {
+            let numeroAleatorio = gerarNumeroAleatorio(1, 75);
+            if (numerosSorteados.includes(numeroAleatorio) == true) {
+                numeroExiste = true;
+            } else {
+                numeroExiste = false;
+                numerosSorteados.push(numeroAleatorio);
+                let divNumero = document.createElement("div");
+                divNumero.classList.add("col-2");
+                divNumero.classList.add("sorteado");
+                divNumero.innerText = numeroAleatorio;
+                divSorteados.appendChild(divNumero);
+                console.log("Números sorteados:", numerosSorteados);
+                vetorJogadores.forEach(function (jogador) {
+                    if (verificaCartela(jogador.cartela, numerosSorteados, 25) == true) {
+                        console.log(`${jogador.nome} ganhou o BINGO! Parabénnnsssssss!!!!!!`);
+                        clearInterval(intervalo);
+                    }
+                });
+            }
+        }
+        if (numerosSorteados.length >= 75) {
+            console.log("Sorteio Finalizado!");
+            clearInterval(intervalo);
+        }
+    }, 1000);
 }
